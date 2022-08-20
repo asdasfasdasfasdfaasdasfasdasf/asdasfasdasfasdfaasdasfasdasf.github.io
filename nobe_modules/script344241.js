3dce886f941e56d5e4073827e16597231772f9c6
Name <82287869+asdasfasdasfasdfaasdasfasdasf@users.noreply.github.com>
2022-08-20 21:10:50 +0200

    Add files via upload

---
+++ nobe_modules/script344241.js
@@ -0,0 +1,41 @@
+//create a new function that will be called when the page is loaded
+window.onload = function() {
+    //create a new variable that will hold the value of the element with the id of "myButton"
+    var myButton = document.getElementById("myButton");
+    //create a new variable that will hold the value of the element with the id of "myText"
+    var myText = document.getElementById("myText");
+    //create a new variable that will hold the value of the element with the id of "myText2"
+    var myText2 = document.getElementById("myText2");
+    //create a new variable that will hold the value of the element with the id of "myText3"
+    var myText3 = document.getElementById("myText3");
+    //create a new variable that will hold the value of the element with the id of "myText4"
+    var myText4 = document.getElementById("myText4");
+    //create a new variable that will hold the value of the element with the id of "myText5"
+    var myText5 = document.getElementById("myText5");
+    //create a new variable that will hold the value of the element with the id of "myText6"
+    var myText6 = document.getElementById("myText6");
+    //create a new variable that will hold the value of the element with the id of "myText7"
+    var myText7 = document.getElementById("myText7");
+    //create a new variable that will hold the value of the element with the id of "myText8"
+    var myText8 = document.getElementById("myText8");
+    //create a new variable that will hold the value of the element with the id of "myText9"
+    var myText9 = document.getElementById("myText9");
+    //create a new variable that will hold the value of the element with the id of "myText10"
+    var myText10 = document.getElementById("myText10");
+    //create a new variable that will hold the value of the element with the id of "myText11"
+    var myText11 = document.getElementById("myText11");
+    //create a new variable that will hold the value of the element with the id of "myText12"
+    var myText12 = document.getElementById("myText12");
+    //create a new variable that will hold the value of the element with the id of "myText13"
+    var myText13 = document.getElementById("myText13");
+    //create a new variable that will hold the value of the element with the id of "myText14"
+    
+    //create a new variable that will hold the value of the element with the id of "myText15"
+    var myText15 = document.getElementById("myText15");
+    //create a new variable that will hold the value of the element with the id of "myText16"
+    var myText16 = document.getElementById("myText16");
+    //create a new variable that will hold the value of the element with the id of "myText17"
+    var myText17 = document.getElementById("myText17");}
+
+
+    myText10.style.display = "none";
